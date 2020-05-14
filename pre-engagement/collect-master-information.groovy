cc273e874576a3754b987246ac50a8a8ad960fed
Darin Pope <darin@planetpope.com>
2020-05-14 15:24:03 -0400

    Update collect-master-information.groovy

--- pre-engagement/collect-master-information.groovy
+++ pre-engagement/collect-master-information.groovy
@@ -3,6 +3,6 @@ import com.cloudbees.jce.masterprovisioning.mesos.MesosMasterProvisioning
 println "name,disk,cpus,memory,ratio,image"
 Jenkins.instance.getAllItems(ManagedMaster.class).each{ managedMaster ->
   MesosMasterProvisioning kmp = (MesosMasterProvisioning) managedMaster.getConfiguration();
-  println managedMaster.name+","+kmp.disk+","+kmp.cpus+","+kmp.memory+","+kmp.ratio+","+managedMaster.configuration.image
+  println managedMaster.name+","+kmp.disk+","+kmp.cpus+","+kmp.memory+","+kmp.ratio+","+kmp.image
 }
 return
